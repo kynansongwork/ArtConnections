66818bed8f3c2d1c27a49610e725691c53dcaca0
Kynan <k.song@blueyonder.co.uk>
2019-09-29 20:01:55 +0100

    Added basic profile structure

--- ArtConnections/UI/ProfileViewController.swift
+++ ArtConnections/UI/ProfileViewController.swift
@@ -11,22 +11,18 @@ import UIKit
 class ProfileViewController: UIViewController, StoryboardLoadedViewController {
     
     var viewModel: ViewModel!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var diciplineLabel: UILabel!
+    @IBOutlet weak var portfolioCollectionView: UICollectionView!
+    @IBOutlet weak var aboutTheUserView: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
