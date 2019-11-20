fcba96fddedc4664453a7e14a72f8bf06a4adb62
Kynan Song <kynan.song@bemo.co>
2019-11-20 16:13:52 +0000

    Now present sign up flow and show additional details.

---
+++ ArtConnections/UI/Sign Up/AdditionalDetailsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AdditionalDetailsViewController.swift
+//  ArtConnections
+//
+//  Created by Kynan Song on 20/11/2019.
+//  Copyright © 2019 scottishAsian. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AdditionalDetailsViewController: KeyboardViewController, StoryboardLoadedViewController {
+    var viewModel: ViewModel!
+    
+    
+}
