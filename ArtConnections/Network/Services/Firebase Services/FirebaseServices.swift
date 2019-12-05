dd3cd5c13a523b327b89d0baad32199bc1e63471
Kynan <k.song@blueyonder.co.uk>
2019-12-05 13:28:45 +0000

    Creating separate branch to explore using firebase for the login flow.

---
+++ ArtConnections/Network/Services/Firebase Services/FirebaseServices.swift
@@ -0,0 +1,13 @@
+//
+//  FirebaseServices.swift
+//  ArtConnections
+//
+//  Created by Kynan Song on 05/12/2019.
+//  Copyright © 2019 scottishAsian. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseServices {
+    
+}
